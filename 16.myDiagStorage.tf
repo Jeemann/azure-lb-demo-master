5cd7c46721f3255275a146e80898976cac1a04e6
Jeemann <90170033+Jeemann@users.noreply.github.com>
2021-09-29 22:09:55 +0900

    Update 16.myDiagStorage.tf

--- 16.myDiagStorage.tf
+++ 16.myDiagStorage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                = "davidvm1diagestorage1"
+    name                = "user25vm1diagestorage1"
     resource_group_name = azurerm_resource_group.user25-rg.name
     location = azurerm_resource_group.user25-rg.location
     account_replication_type = "LRS"
